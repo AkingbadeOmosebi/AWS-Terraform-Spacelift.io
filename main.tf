da36197ca274c0ac4ad2baa575410b2f453acc68
AkingbadeOmosebi <akingbadeomosebi@gmail.com>
2025-06-27 01:11:22 +0200

    added more resources and updated readme

---
+++ main.tf
@@ -0,0 +1,40 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "my-simple-vpc"
+  }
+}
+
+
+# Subnet -1 
+resource "aws_subnet" "my_vpc" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet-1"
+  }
+}
+
+# Subnet -2
+resource "aws_subnet" "my_vpc" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "subnet-2"
+  }
+}
+
+
+# Subnet -3
+resource "aws_subnet" "my_vpc" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "subnet-3"
+  }
+}
